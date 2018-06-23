661e82c8ad61ea740e1d6024a6362d34cd36ab28
ErickFraga <37560574+ErickFraga@users.noreply.github.com>
2018-06-22 23:11:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,56 @@
+var C, T, I, M, inicial, rendimento, jurosTotal = 0, total = 0, a;
 
- var C, T, I, M, inicial,rendimento, jurosTotal = 0, total = 0;
-function Atribuir(){
+var preenchido = new Array(false, false, false, false);
+function Atribuir() {
     document.getElementById("resultado").innerHTML = "";
     C = document.getElementById("C").value;
     T = document.getElementById("T").value;
     I = document.getElementById("I").value;
 
-    C = parseFloat( C, 10);
-    T = parseFloat( T, 10);
-    I = parseFloat( I, 10);
-
-    inicial = C
-    for(j=1; j<=T; j++){
-        Calcular(C, I, j);
+    for(i=0; i<preenchido.length; i++){
+        if(preenchido[i]==true){
+         a = true   
+        }else{
+            a=false
+            break;
+        }
     }
-    jurosTotal = M - C
-    rendimento = (jurosTotal*100)/M;   
-    document.getElementById("resultado").innerHTML += "<div class='periodo' id='Juros'> Total de rendimento R$" + jurosTotal.toFixed(2) + "</div>";
-    document.getElementById("resultado").innerHTML += "<div class='periodo' id='Jprocent'> Porcentagem total de remdimento " + rendimento.toFixed(2) + "% </div>";
 
+    if(!a){
+        alert("Preencha todos os campos");
+    }else{
+        C = parseFloat(C, 10); 
+        T = parseFloat(T, 10);
+        I = parseFloat(I, 10);
+        inicial = C
+        
+        ChekPeriodo();
+        
+        for (j = 1; j <= T; j++) {
+            Calcular(C, I, j);
+        }
+        jurosTotal = M - C
+        rendimento = (jurosTotal * 100) / M;
+        document.getElementById("resultado").innerHTML += "<div class='periodo' id='Juros'> Total de rendimento R$" + jurosTotal.toFixed(2) + "</div>";
+        document.getElementById("resultado").innerHTML += "<div class='periodo' id='Jprocent'> Porcentagem total de remdimento " + rendimento.toFixed(2) + "% </div>";
+        
+        document.getElementById("resultado").style.border = "2px solid black";
+    }
 }
-
-function Calcular(inicial, juros, i){
-    var porJuros = Math.pow((1+ (juros/100)), i);
+    
+    function Calcular(inicial, juros, i) {
+        var porJuros = Math.pow((1 + (juros / 100)), i);
     M = inicial * porJuros;
     M = M.toFixed(2);
-    document.getElementById("resultado").innerHTML += "<div class='parcela' id='periodo" + i + "'>" + i + "º Mês R$" + M + "</div>";
+    document.getElementById("resultado").innerHTML += "<div class='parcela' id='periodo" + i + "'>" + i + "º " + periodo + " R$" + M + "</div>";
 
-}
+} 
+
+function ChekPeriodo(){
+    var rad = document.getElementsByName("gender") ;
+    for(i=0; i<3; i++){
+        if(rad[i].checked == true){
+           periodo = rad[i].value;
+        }
+    }
+} 
